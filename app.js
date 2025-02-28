29c28ede6239f49f8c2b60acef4e9198443f5896
jaypawar2004 <pawarjay7000@gmail.com>
2025-02-28 23:16:41 +0530

    app.js

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
+// const express = require("express");
+// require("dotenv").config({path : "./.env"})
+
+// const app = express();
+
+// // Logger
+// const logger = require("morgan");
+// app.use(logger("tiny"))
+
+// // Routes Code
+// app.use("/", require("./routes/indexRouter"))
+
+// //  error handling
+
+// const ErrorHandler = require("./utils/ErrorHandler");
+// app.all("*", (req, res , next)=> {
+//     next(new ErrorHandler(`url not found ${req.url}`, 404))
+// })
+
+// app.listen(process.env.PORT ,
+//      console.log(`sever running port ${process.env.PORT}`))
+
 const express = require("express");
 require("dotenv").config({path : "./.env"})
+const logger = require("morgan")
 
 const app = express();
-
-// Logger
-const logger = require("morgan");
 app.use(logger("tiny"))
+app.get("/", (req, res) => {
+    res.send("Hello dosto");
 
-// Routes Code
-app.use("/", require("./routes/indexRouter"))
 
-//  error handling
-
-const ErrorHandler = require("./utils/ErrorHandler");
-app.all("*", (req, res , next)=> {
-    next(new ErrorHandler(`url not found ${req.url}`, 404))
 })
 
-app.listen(process.env.PORT ,
-     console.log(`sever running port ${process.env.PORT}`))
+app.listen(process.env.PORT, 
+    console.log(`Server running on ${process.env.PORT}`))
