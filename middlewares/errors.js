82978914978ddf9332faa961912f3be0e35436bc
jaypawar2004 <pawarjay7000@gmail.com>
2025-03-01 22:59:19 +0530

    err

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,6 +1,6 @@
 exports.generatedErrors = (err, req, res, next ) => {
 const statusCode = err.statusCode || 500;
-
+// error 
 res.status(statusCode).json({
     message: err.message,
     errName: err.name,
